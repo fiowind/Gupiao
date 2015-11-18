d0961e48782731238610f45e60124ee4aa5fb6e8
zhoufangxing <fio@zhoufangxingdeMacBook-Pro.local>
2015-11-18 15:35:30 +0800

    a

--- climbgupiao/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from models import gupiaolist, bankuailist
-admin.site.register(gupiaolist)
-admin.site.register(bankuailist)
--- climbgupiao/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-from climbgupiao import views
-
- 
-urlpatterns = patterns('',
-    url(r'^$', views.search, name='search'),
-    url(r'^search/$',views.search,name = 'search'),
-)
